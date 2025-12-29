bdd09117096018676147eedea0c09bc85c923315
dhushan7 <it23617414@my.sliit.lk>
2025-12-29 18:00:26 +0530

    loading screen and nav bar has completed

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import "./index.css"
+import { LoadingScreen } from './components/LoadingScreen';
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"}`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      </div>
+    </>
+  )
+}
+
+export default App;
